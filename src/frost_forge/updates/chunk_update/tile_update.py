417d01db764a032b7f654df180a1cd2460f597a6
TimoAndy08 <timsor08@gmail.com>
2025-07-16 18:46:11 +0200

    Moved grow into plant

--- src/frost_forge/updates/chunk_update/tile_update.py
+++ src/frost_forge/updates/chunk_update/tile_update.py
@@ -1,16 +1,11 @@
-from random import randint
-
 from .point import left, up
 from .rabbit import rabbit_hole, rabbit_entity
 from .machine import machine
-from ...info import GROW_CHANCE, GROW_TILES
-from ...tile_systems.tile_class import Tile
+from .plant import grow
 
 def update_tile(current_tile, chunks, chunk, tile, delete_tiles, create_tiles, tick):
     if "grow" in current_tile.attributes:
-        if randint(0, GROW_CHANCE[current_tile.kind]) == 0:
-            grow_tile = GROW_TILES[current_tile.kind]
-            chunks[chunk][tile] = Tile(grow_tile[0], grow_tile[1], current_tile.floor, floor_health = current_tile.floor_health, floor_unbreak = current_tile.floor_unbreak, spawn = current_tile.spawn)
+        chunks[chunk][tile] = grow(current_tile)
     elif current_tile.kind == "left":
         chunks, delete_tiles = left(chunks, chunk, tile, delete_tiles)
     elif current_tile.kind == "up":
