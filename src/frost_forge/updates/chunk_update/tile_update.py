3df0eaf6fc05b1e543f74d7c3cdc588f2d51c3a2
TimoAndy08 <timsor08@gmail.com>
2025-10-14 17:11:41 +0200

    Update check change
    - All the chunk updates now test 6 times per second (Massive performance boost)

--- src/frost_forge/updates/chunk_update/tile_update.py
+++ src/frost_forge/updates/chunk_update/tile_update.py
@@ -22,33 +22,32 @@ def update_tile(
         chunks, delete_tiles = left(chunks, chunk, tile, delete_tiles)
     elif current_tile["kind"] == "up":
         chunks, delete_tiles = up(chunks, chunk, tile, delete_tiles)
-    elif tick % (FPS // 6) == 0:
-        if "machine" in ATTRIBUTES.get(current_tile["kind"], ()) and tick % PROCESSING_TIME[current_tile["kind"]] == 0:
-            chunks[chunk][tile]["inventory"] = recipe(current_tile["kind"], current_tile.get("recipe", 0), current_tile.get("inventory", {}))
-        elif current_tile["kind"] in GROW_TILES:
-            chunks[chunk][tile] = grow(current_tile)
-            if chunks[chunk][tile] == {}:
-                delete_tiles.append((chunk, tile))
-        elif "animal" in ATTRIBUTES.get(current_tile["kind"], ()):
-            create_tiles, delete_tiles = animal(
-                chunks,
-                chunk,
-                tile,
-                current_tile,
-                create_tiles,
-                delete_tiles,
-                location,
-                inventory_key,
-            )
-        elif "enemy" in ATTRIBUTES.get(current_tile["kind"], ()):
-            create_tiles, delete_tiles, health = enemy(
-                chunks,
-                chunk,
-                tile,
-                current_tile,
-                create_tiles,
-                delete_tiles,
-                location,
-                health,
-            )
+    elif "machine" in ATTRIBUTES.get(current_tile["kind"], ()) and tick % PROCESSING_TIME[current_tile["kind"]] == 0:
+        chunks[chunk][tile]["inventory"] = recipe(current_tile["kind"], current_tile.get("recipe", 0), current_tile.get("inventory", {}))
+    elif current_tile["kind"] in GROW_TILES:
+        chunks[chunk][tile] = grow(current_tile)
+        if chunks[chunk][tile] == {}:
+            delete_tiles.append((chunk, tile))
+    elif "animal" in ATTRIBUTES.get(current_tile["kind"], ()):
+        create_tiles, delete_tiles = animal(
+            chunks,
+            chunk,
+            tile,
+            current_tile,
+            create_tiles,
+            delete_tiles,
+            location,
+            inventory_key,
+        )
+    elif "enemy" in ATTRIBUTES.get(current_tile["kind"], ()):
+        create_tiles, delete_tiles, health = enemy(
+            chunks,
+            chunk,
+            tile,
+            current_tile,
+            create_tiles,
+            delete_tiles,
+            location,
+            health,
+        )
     return chunks, create_tiles, delete_tiles, health
