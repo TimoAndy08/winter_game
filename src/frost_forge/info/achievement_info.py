c3f0ffbd5f6ec0499f4d57d2c4760c957c384ba2
TimoAndy08 <timsor08@gmail.com>
2025-10-29 13:43:22 +0100

    An assortment of them
    - Like quite a lot of Stnemeveihca

--- src/frost_forge/info/achievement_info.py
+++ src/frost_forge/info/achievement_info.py
@@ -1,3 +1,9 @@
 ACHIEVEMENTS = {
-    "log": ("Chop down a treeling", "Did you except to chop down big trees with those hands?")
+    "log": ("Not real trees", "Did you except to chop down big trees with those hands?"),
+    "flint axe": ("Tree-chopper 3000", "Now, go wreck some trees"),
+    "rabbit fur": ("You monster", "Was that really necessary?"),
+    "wooden bed": ("Go get some sleep", "zzzzZzzZzZzzzZzzzzz"),
+    "life crystal": ("Increasing love", "That's what lv stands for"),
+    "slime crystal": ("Slime slayer", "Not a ninja in sight"),
+    "copper ingot": ("Shiny things", "Need a lot of them you will"),
 }
--- src/frost_forge/render/user_interface_rendering/achievement_rendering.py
+++ src/frost_forge/render/user_interface_rendering/achievement_rendering.py
@@ -1,6 +1,6 @@
 import pygame as pg
 
-from ...info import ACHIEVEMENTS, SLOT_SIZE, HALF_SCREEN_SIZE, UI_SCALE, FLOOR, TILE_UI_SIZE, FLOOR_SIZE
+from ...info import ACHIEVEMENTS, SLOT_SIZE, HALF_SCREEN_SIZE, UI_SCALE, FLOOR, TILE_UI_SIZE, FLOOR_SIZE, UI_FONT
 
 
 def render_achievement(achievement, window, images):
@@ -18,4 +18,6 @@ def render_achievement(achievement, window, images):
             pg.transform.scale(images[achievement], FLOOR_SIZE),
             (HALF_SCREEN_SIZE - 24 * UI_SCALE, 8 * UI_SCALE),
         )
+    window.blit(UI_FONT.render(ACHIEVEMENTS[achievement][0], False, (19, 17, 18)), (HALF_SCREEN_SIZE, 0))
+    window.blit(UI_FONT.render(ACHIEVEMENTS[achievement][1], False, (19, 17, 18)), (HALF_SCREEN_SIZE - 32 * UI_SCALE, 32 * UI_SCALE))
     return window
