3a475d3c365c754b8f152c7bdacaa5622ae657a6
TimoAndy08 <timsor08@gmail.com>
2026-01-08 18:59:39 +0100

    Efficiency change
    - Machines keep internal count of when to check for if they should craft
    - Efficiency divides time count instead of increasing bulk gain
    - 387th time putting a comma after paranthases to show that there is a tuple tuple

--- src/frost_forge/updates/chunk_update/machine.py
+++ src/frost_forge/updates/chunk_update/machine.py
@@ -2,7 +2,7 @@ from ..left_click import recipe
 from .connection import connect_machine
 from .mana import mana_level
 from .transport import output_transport
-from ...info import RUNES_USER, PROCESSING_TIME, CONNECTIONS, FPS
+from ...info import RUNES_USER, CONNECTIONS, PROCESSING_TIME
 
 
 def machine(tick, current_tile, kind, attributes, tile, chunk, chunks):
@@ -12,7 +12,9 @@ def machine(tick, current_tile, kind, attributes, tile, chunk, chunks):
         machine_inventory = current_tile["inventory"]
     if "harvester" in attributes:
         chunks[chunk][tile]["recipe"] = 0
-    if tick % PROCESSING_TIME.get(kind, FPS) == 0 and current_tile.get("recipe", -1) >= 0:
+    if "process" not in current_tile:
+        current_tile["process"] = 0
+    if current_tile.get("recipe", -1) >= 0 and current_tile["process"] == 0:
         delete_item = []
         for item in machine_inventory:
             if item.split(" ")[-1] == "mineable":
@@ -25,8 +27,10 @@ def machine(tick, current_tile, kind, attributes, tile, chunk, chunks):
             machine_inventory, efficiency = mana_level(chunks, chunk, tile, kind, current_tile, machine_inventory, efficiency)
         if kind in CONNECTIONS:
             connection, efficiency, chunks = connect_machine(chunks, chunk, tile, kind, attributes, connection, efficiency)
-        if connection:
-            for _ in range(0, efficiency):
-                machine_inventory = recipe(kind, current_tile["recipe"], machine_inventory, (20, 64))
+        if connection and efficiency:
+            machine_inventory = recipe(kind, current_tile["recipe"], machine_inventory, (20, 64))
+            current_tile["process"] = PROCESSING_TIME[current_tile["kind"]] // efficiency
     machine_inventory, chunks = output_transport(chunks, chunk, tile, current_tile, kind, {"connected", "transport"})
+    if current_tile["process"] > 0:
+        current_tile["process"] -= 1
     return machine_inventory
